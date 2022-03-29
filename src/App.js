2eed4e1d6c240275f3d0a8017ddf30ac98b5d36d
unknown <khan.nawaz790@gmail.com>
2022-03-29 10:29:50 +0200

    create landing page for book collection app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import React from 'react'
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: ""
+    }    
+  }
+  render() {
+    return (
+      <div style={{textAlign: "center"}}>
+        <h1>My Book Collection</h1>
+        <input 
+          type={"text"} value={this.state.inputValue} 
+          placeholder={"Enter book title"}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
